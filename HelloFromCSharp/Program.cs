483c486d7f7ca0c24ef4c64f05c5c79e76092d98
Jayden <Jayden.vinh.nguyen@gmail.com>
2024-08-05 00:40:06 -0700

    removed boilerplate

--- HelloFromCSharp/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Console.WriteLine("Please enter you name: ");
-
-string name = Console.ReadLine();
-
-Console.WriteLine("Hello " + name + "!");
