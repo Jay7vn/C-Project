3ff4d794d79846e278ee85de71030c64d3506ecb
Jayden Nguyen <Jayden.vinh.nguyen@gmail.com>
2024-08-05 01:12:43 -0700

    Updated program.cs

--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+Console.WriteLine("Please enter you name: ");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Hello " +  name + "!");
+
 Console.ReadLine();
