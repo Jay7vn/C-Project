2c51f63c69c58478ff9528d6e420ecc63cf4e912
Jayden <Jayden.vinh.nguyen@gmail.com>
2024-08-05 00:28:27 -0700

    Add user input functionality to HelloFromCSharp program

--- HelloFromCSharp/obj/Debug/net8.0/HelloFromCSharp.AssemblyInfo.cs
+++ HelloFromCSharp/obj/Debug/net8.0/HelloFromCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloFromCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61a393c9e223eadb12ea7ff5d7f13af781e0282a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6facaf5e5bf560aaa0ef327141dc9a8cd683080")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloFromCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloFromCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
