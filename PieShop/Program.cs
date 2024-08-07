c5bba5c8cb4ded457ec35066d629269b229d369e
Jayden Nguyen <Jayden.vinh.nguyen@gmail.com>
2024-08-06 22:23:25 -0700

    Variables added

---
+++ PieShop/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Welcome to our Pie Shop!");
+
+Console.WriteLine("Please enter your name: ");
+
+string name = Console.ReadLine();
+
+int monthlyWage = 1234;
+
+int months = 12, bonus = 1000;
+
+bool isActive = true;
+
+double rating = 99.25;
+
+//byte numberOfEmployees = 125;
+//byte can only contain 0-255
+
+int hoursWorked;
+
+hoursWorked = 125;
+
+const double interestRate = 0.07;
+
+String firstName = "Jayden";
+String lastName = "Nguyen";
